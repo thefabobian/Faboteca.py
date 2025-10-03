ca792f7105bf1ae79dcc936e079264956c28b806
Fabian Espinosa <fabianespinosapastrana@gmail.com>
2025-10-03 12:24:43 -0500

    metodo main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-nombre = input('Cuál es tu nombre? ')
-print('Hola', nombre, 'es un placer saludarlo!')
+import tkinter as tk
+from view.usuario_view import UsuarioView
+from controller.usuario_controller import UsuarioController
+
+def main():
+    root = tk.Tk()
+    controlador = UsuarioController()
+    UsuarioView(root, controlador)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
